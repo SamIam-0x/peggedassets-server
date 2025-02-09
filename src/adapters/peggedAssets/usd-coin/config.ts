3fa63ea82c41ab57d40fe8c70f7794a5fb9f2e9e
Sam <sam.tolomei@circle.com>
2025-02-08 22:17:30 -0800

    Update config.ts

--- src/adapters/peggedAssets/usd-coin/config.ts
+++ src/adapters/peggedAssets/usd-coin/config.ts
@@ -408,4 +408,19 @@ export const chainContracts: ChainContracts = {
   soneium: {
     bridgedFromETH: ["0xbA9986D2381edf1DA03B0B9c1f8b00dc4AacC369"],
   },
+  berachain: {
+    bridgedFromETH: ["0x549943e04f40284185054145c6e4e9568c1d3241"],
+  },
+  core: {
+    bridgedFromETH: ["0xa4151b2b3e269645181dccf2d426ce75fcbdeca9"],
+  },
+  cronos: {
+    bridgedFromETH: ["0xc21223249ca28397b4b6541dffaecc539bff0c59"],
+  },
+  b_squared: {
+    bridgedFromETH: ["0xE544e8a38aDD9B1ABF21922090445Ba93f74B9E5"],
+  },
+  bob: {
+    bridgedFromETH: ["0xe75D0fB2C24A55cA1e3F96781a2bCC7bdba058F0"],
+  },
 };
