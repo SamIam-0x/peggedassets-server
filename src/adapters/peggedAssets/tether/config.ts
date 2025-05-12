0a54afdce924bc9d4e7557a798e318f6edc43720
Sam <sam.tolomei@circle.com>
2025-05-11 17:31:32 -0700

    updating config for hl

--- src/adapters/peggedAssets/tether/config.ts
+++ src/adapters/peggedAssets/tether/config.ts
@@ -387,4 +387,7 @@ export const chainContracts: ChainContracts = {
   flare: { 
     bridgedFromETH: ["0xe7cd86e13AC4309349F30B3435a9d337750fC82D"], // USDT0
   },
+  hyperliquid: { 
+    bridgedFromETH: ["0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb"], // USDT0
+  },
 };
